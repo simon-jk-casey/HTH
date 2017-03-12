5d8fb3b02b37bdd42d7fe274e375ac9f80161ec6
simon-jk-casey <jesusiwasevil@gmail.com>
2017-03-13 12:31:17 +1300

    setup API routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
-});
+
+router.post('/signup', (req, res) => {
+  //username
+  //password
+  //first name
+  //last name
+  //address
+  //gps coordinates
+  //number of devices
+  //device types
+})
+
+router.get('/users/:id', (req, res) => {
+  //display authenticated user data
+})
+
+router.post('/users/:id', (req, res) => {
+  //edit user data
+})
+
+router.post('/captures', (req, res) => {
+  //capture type (trapped/detected?)
+  //datestamp
+  //notes
+})
+
+router.get('/captures')
 
 module.exports = router;
