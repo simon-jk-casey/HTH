c4470535af53c1278649a1a9c25bf7e8f50e608d
simon-jk-casey <jesusiwasevil@gmail.com>
2017-03-14 14:48:23 +1300

    completed table migrations

--- migrations/20170313124256_predatorData.js
+++ migrations/20170313124256_predatorData.js
@@ -8,9 +8,8 @@ exports.up = function(knex, Promise) {
     table.string('notes')
     table.timestamp('captureDate').defaultTo(knex.fn.now())
   })
-
-};
+}
 
 exports.down = function(knex, Promise) {
-
-};
+  return knex.schema.dropTableIfExists('predatorData')
+}
