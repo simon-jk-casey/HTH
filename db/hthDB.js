df5b1b3c13dfdab161920865f1fa9a25c8ec1243
simon-jk-casey <jesusiwasevil@gmail.com>
2017-03-14 15:02:17 +1300

    setup basic functions for signup, user retreival, device entry and retrieval

--- db/hthDB.js
+++ db/hthDB.js
@@ -1,3 +1,41 @@
 const Knex = require('knex')
 const knexConfig = require('../knexfile')[process.env.NODE_ENV || 'development']
 const knex = Knex(knexConfig)
+
+function addUser (user) {
+  return knex('users').insert(user)
+}
+
+function getUsers () {
+  return knex('users')
+}
+
+function getUserByUsername (username) {
+  return knex('users').where('username', `${username}`)
+}
+
+function getUserById (id) {
+  return knex('users').where('id', `${id}`)
+}
+
+function addDevice (device) {
+  return knex('devices').insert(device)
+}
+
+function getDevices () {
+  return knex('devices')
+}
+
+function getDeviceById (id) {
+  return knex('devices').where('id', `${id}`)
+}
+
+module.exports = {
+  addUser,
+  getUsers,
+  getUserByUsername,
+  getUserById,
+  addDevice,
+  getDevices,
+  getDeviceById
+}
