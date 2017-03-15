3597987e3c4a3845825749b43a55ecf76f7f243e
simon-jk-casey <jesusiwasevil@gmail.com>
2017-03-15 15:41:02 +1300

    add post route for /devices; adds new devices to user acc, tested - working with postman

--- routes/api.js
+++ routes/api.js
@@ -6,21 +6,59 @@ const db = require('../db/hthDB')
 router.post('/signup', (req, res) => {
   //add street type
   const {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords} = req.body
-  var user = {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords}
+  const user = {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords}
   db.addUser(user)
   .then((res) => {
     console.log(res, "result")
-    //logs user table id
-    //handle this and error when p/w hashing setup
   })
+  .catch((err) => {
+    console.log(err);
+  })
+})
+
+router.post('/login', (req, res) => {
+  //get login details to compare
+  //complete when passport auth set up
 })
 
 router.get('/users/:id', (req, res) => {
   //display authenticated user data
+
 })
 
 router.post('/users/:id', (req, res) => {
-  //edit user data
+  //edit user info
+})
+
+router.get('/devices', (req, res) => {
+  db.getDevices()
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+router.post('/devices', (req, res) => {
+  // const {userId, deviceType} = req.body
+  // const device = {userId, deviceType}
+  //userId will come from elsewhere when passport added, need to fix then (can also be non-int but since will be supplied by cookie for auth user shouldnt matter as will be auto)
+  db.addDevice(req.body)
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+router.get('/devices/:id', (req, res) => {
+  //get specific device details by id
+})
+
+router.post('/devices/:id', (req, res) => {
+  //remove a device
 })
 
 router.post('/captures', (req, res) => {
@@ -34,6 +72,6 @@ router.get('/captures', (req, res) => {
 })
 
 
-//ADD DEVICE ROUTES
+//REMOVE CONSOLE LOGS, ADD ERROR HANDLING
 
 module.exports = router;
