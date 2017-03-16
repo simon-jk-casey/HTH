535e51e5fca96feef09ae8f73e6a5b71cd385a7d
simon-jk-casey <jesusiwasevil@gmail.com>
2017-03-16 15:39:51 +1300

    add pw hash to signup, passport auth (local strat) to login, add ensureAuthenticated fn to appropriate routes

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,59 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const passport = require('../passportSetup')
 
 const db = require('../db/hthDB')
 
+const saltRounds = 10
+
+function ensureAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    res.send('Unauthorised')
+  }
+}
+
 router.post('/signup', (req, res) => {
   // add street type
-  // const {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords} = req.body
-  // const user = {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords}
-  // WILL LIKELY TO RE_ADD ABOVE ONCE HASHING IMPLEMENTED
-  db.addUser(req.body)
-  .then((res) => {
-  })
-  .catch((err) => {
-    console.log(err)
+  const {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords} = req.body
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) throw err
+    else {
+      const user = {username, password, firstName, lastName, streetNumber, streetName, suburb, city, email, gpsCoords}
+      db.addUser(user)
+      .then(() => res.json({status: 200, message: 'signup successful'}))
+      .catch((err) => {
+        throw err
+      })
+    }
   })
 })
 
-router.post('/login', (req, res) => {
-  // get login details to compare
-  // complete when passport auth set up
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json({user: req.user})
 })
 
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', ensureAuthenticated, (req, res) => {
   // display authenticated user data
-
 })
 
-router.post('/users/:id', (req, res) => {
+router.post('/users/:id', ensureAuthenticated, (req, res) => {
   // edit user info
 })
 
-router.get('/devices', (req, res) => {
+router.get('/devices', ensureAuthenticated, (req, res) => {
   db.getDevices()
   .then((result) => {
     res.json(result)
-    // returning object correctly, handle as necessary for front end
   })
   .catch((err) => {
     console.log(err)
   })
 })
 
-router.post('/devices', (req, res) => {
+router.post('/devices', ensureAuthenticated, (req, res) => {
   // const {userId, deviceType} = req.body
   // const device = {userId, deviceType}
   // userId will come from elsewhere when passport added, need to fix then (can also be non-int but since will be supplied by cookie for auth user shouldnt matter as will be auto)
@@ -54,7 +66,7 @@ router.post('/devices', (req, res) => {
   })
 })
 
-router.get('/devices/:id', (req, res) => {
+router.get('/devices/:id', ensureAuthenticated, (req, res) => {
   db.getDeviceById(req.params.id)
   .then((result) => {
     res.json(result)
@@ -64,7 +76,7 @@ router.get('/devices/:id', (req, res) => {
   })
 })
 
-router.post('/devices/:id', (req, res) => {
+router.post('/devices/:id', ensureAuthenticated, (req, res) => {
   db.removeDevice(req.params.id)
   .then((result) => {
     console.log(result)
@@ -74,7 +86,7 @@ router.post('/devices/:id', (req, res) => {
   })
 })
 
-router.post('/captures', (req, res) => {
+router.post('/captures', ensureAuthenticated, (req, res) => {
   db.addPredatorData(req.body)
   .then((result) => {
     console.log(result)
@@ -84,7 +96,7 @@ router.post('/captures', (req, res) => {
   })
 })
 
-router.get('/captures', (req, res) => {
+router.get('/captures', ensureAuthenticated, (req, res) => {
   db.getPredatorData()
   .then((result) => {
     res.json(result)
