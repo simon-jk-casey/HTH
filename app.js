abed01904afc7d888494a755764614aeab2329d7
simon-jk-casey <jesusiwasevil@gmail.com>
2017-03-16 15:15:29 +1300

    create passportSetup

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-var express = require('express')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-var api = require('./routes/api')
+const api = require('./routes/api')
+const passport = require('./passportSetup')
 
-var app = express()
+const corsOptions = {
+  origin: true,
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  preflightContinure: false,
+  credentials: true
+}
 
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(require('express-session')({secret: 'pr3d4t0r', resave: false, saveUninitialized: false}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/api/v1', api)
 
