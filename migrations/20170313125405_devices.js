1e82cff1a8fa3ee8b9e7b7f087112b53e110abd9
simon-jk-casey <jesusiwasevil@gmail.com>
2017-04-04 14:54:15 +1200

    add device name and notes columns to device table migration

--- migrations/20170313125405_devices.js
+++ migrations/20170313125405_devices.js
@@ -3,7 +3,9 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('devices', (table) => {
     table.increments('id').primary()
     table.integer('userId').notNullable()
+    table.string('deviceName').notNullable()
     table.string('deviceType').notNullable()
+    table.string('notes')
   })
 }
 
